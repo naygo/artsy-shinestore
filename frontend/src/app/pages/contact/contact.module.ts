dce4b65f9ff3d0a5ab8e22ee3464866fa69ce7f1
Nayla Gomes <nayla.cgs@gmail.com>
2021-03-02 22:30:14 -0300

    Ajuste na tela de contato e personalização dos textarea

--- frontend/src/app/pages/contact/contact.module.ts
+++ frontend/src/app/pages/contact/contact.module.ts
@@ -2,8 +2,7 @@ import { ContactComponent } from './contact.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputTextModule } from 'primeng/inputtext';
-
-
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 @NgModule({
   declarations: [
@@ -11,7 +10,8 @@ import { InputTextModule } from 'primeng/inputtext';
   ],
   imports: [
     CommonModule,
-    InputTextModule
+    InputTextModule,
+    InputTextareaModule
   ],
   exports: [
     ContactComponent
